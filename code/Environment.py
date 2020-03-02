d4d5155e9f2e659d089f1210f42c563698c9b8da
kartikv97 <56460840+kartikv97@users.noreply.github.com>
2020-03-02 17:16:38 -0500

    Update Environment.py
    --Added function to detect obstacles.
    --Added function to draw explored nodes.

--- code/Environment.py
+++ code/Environment.py
@@ -1,6 +1,8 @@
 
 
 import pygame
+import pygame.gfxdraw
+import time
 
 pygame.init()
 
@@ -27,6 +29,29 @@ pygame.draw.circle(gameDisplay, white, (160*scale,50*scale), 15*scale)
 
 # pygame.draw.polygon(gameDisplay, white, ((25,75),(76,125),(250,375),(400,25),(60,540)))
 
+def check_Obstacle(x, y):
+#   For square
+    if x>=90 and x<=110 and y>=40 and y<=60:
+        return  True
+#   For Circle
+    if (x-160)**2 + (y-50)**2 < 15**2:
+        return True
+    else:
+        return False
+
+# if check_Obstacle(109,60):
+#     print("Obstacle detected!!")
+# else:
+#     print("No Obstacle!!")
+
+def draw_Explored_Nodes(x,y):
+    pygame.gfxdraw.pixel(gameDisplay,x,y,red)
+    pygame.display.update()
+    # return None
+# for  x in range (50):
+#     for y in range (100):
+#         draw_Explored_Nodes(x,y)
+        # time.sleep()
 
 while True:
     for event in pygame.event.get():
@@ -35,3 +60,6 @@ while True:
             quit()
 
     pygame.display.update()
+
+
+
